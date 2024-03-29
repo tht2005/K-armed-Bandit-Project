e357b964c566959cce93bb63eacf9b745140c08b
Dang Duong <dangduong31205@gmail.com>
2024-03-30 04:07:44 +0700

    change distribution

--- env.py
+++ env.py
@@ -2,18 +2,25 @@ import numpy as np
 import sys
 
 class Env:
-    def initTest(self, N, MAXR, COST):
-        self.A          = N
-        self.PROB       = [ ( (p / 2) if (p > 0.5) else p ) for p in np.random.rand(N) ]
-        self.R          = np.random.randint(0, MAXR, size=N)
-        self.COST       = COST
+    def initTest(self, A):
+        self.A          = A
+
+        self.LOW        = -4
+        self.HIGH       = 3
+
+        self.E          = [ np.random.dirichlet(np.ones(self.HIGH - self.LOW + 1)) for _ in range(A) ]
 
     def sendAction(self, a):
         if a < 0 or a >= self.A:
             print('Undefined action: {}'.format(a))
             sys.exit()
 
-        random_number = np.random.rand()
-        if random_number <= self.PROB[a]:
-            return self.R[a] - self.COST
-        return -self.COST
+        random_value = np.random.rand()
+        # reward
+        for i in range(len(self.E[a])):
+            random_value -= self.E[a][i]
+            if random_value <= 0:
+                return self.LOW + i
+            
+        return self.HIGH
+
