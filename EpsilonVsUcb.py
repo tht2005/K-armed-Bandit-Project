8f732c2032bf420a406c33c5f1a4a51c14f9b531
Dang Duong <dangduong31205@gmail.com>
2024-03-30 06:28:21 +0700

    E VS USB

---
+++ EpsilonVsUcb.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from epsilongreedy import EpsilonGreedy
+from ucb import Ucb
+from env import Env
+
+#constants
+NTEST = 2000
+NSTEP = 1000
+NACTION = 10
+
+UCB_C = 2
+
+# environment
+env = Env()
+
+# history
+history = [ np.array([0] * (NSTEP + 1), dtype=float) for first_d_size in range(2) ]
+
+for itest in range(NTEST):
+    env.initTest(NACTION)
+
+    e = EpsilonGreedy(env=env, epsilon=0.1, NSTEP=NSTEP)
+    history[0] += np.array(e.solve())
+
+    u = Ucb(env=env, c=UCB_C, NSTEP=NSTEP)
+    history[1] += np.array(u.solve())
+
+    print('\rTEST: ', itest + 1, end='')
+
+print()
+
+
+# Draw graph
+plt.xlabel('Time steps')
+plt.ylabel('Average Reward')
+
+step = [ i for i in range(NSTEP + 1) ]
+plt.plot(step, history[0] / NTEST, label='e = 0.1', linestyle='--')
+plt.plot(step, history[1] / NTEST, label='c = 2', linestyle=':')
+
+plt.legend()
+plt.show()
+
