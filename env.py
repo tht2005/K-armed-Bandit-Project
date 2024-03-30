9de689eb894c77043b7a4965b6f52c12010b0426
Dang Duong <dangduong31205@gmail.com>
2024-03-31 00:15:26 +0700

    change env

--- env.py
+++ env.py
@@ -5,22 +5,17 @@ class Env:
     def initTest(self, A):
         self.A          = A
 
-        self.LOW        = -4
-        self.HIGH       = 3
+        self.LOW        = -5
+        self.HIGH       = 1
+        self.MAXRANGE   = 7
 
-        self.E          = [ np.random.dirichlet(np.ones(self.HIGH - self.LOW + 1)) for _ in range(A) ]
+        self.EV         = [ np.random.uniform(low=self.LOW, high=self.HIGH) for i in range(A) ]
+        self.RANGE      = [ np.random.uniform(low=0, high=self.MAXRANGE) for i in range(A) ]
 
     def sendAction(self, a):
         if a < 0 or a >= self.A:
             print('Undefined action: {}'.format(a))
             sys.exit()
 
-        random_value = np.random.rand()
-        # reward
-        for i in range(len(self.E[a])):
-            random_value -= self.E[a][i]
-            if random_value <= 0:
-                return self.LOW + i
-            
-        return self.HIGH
+        return np.random.normal(loc=self.EV[a], scale=self.RANGE[a])
 
