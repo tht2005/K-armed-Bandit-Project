3a288b1266caf5735ecf8ad39b13ee26f75c1955
Dang Duong <dangduong31205@gmail.com>
2024-03-31 12:12:35 +0700

    change env -> normal distribution

--- env.py
+++ env.py
@@ -4,18 +4,12 @@ import sys
 class Env:
     def initTest(self, A):
         self.A          = A
-
-        self.LOW        = -5
-        self.HIGH       = 1
-        self.MAXRANGE   = 7
-
-        self.EV         = [ np.random.uniform(low=self.LOW, high=self.HIGH) for i in range(A) ]
-        self.RANGE      = [ np.random.uniform(low=0, high=self.MAXRANGE) for i in range(A) ]
+        self.EV         = [ np.random.normal(loc=0, scale=1) for i in range(A) ]
 
     def sendAction(self, a):
         if a < 0 or a >= self.A:
             print('Undefined action: {}'.format(a))
             sys.exit()
 
-        return np.random.normal(loc=self.EV[a], scale=self.RANGE[a])
+        return np.random.normal(loc=self.EV[a], scale=1)
 
